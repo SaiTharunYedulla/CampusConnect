d1432b3bcdce09e61af96dd23814b42b97737bc1
SaiTharunYedulla <saitharunyedulla@gmail.com>
2024-10-23 18:20:26 +0530

    Add files via upload

---
+++ Signin.js
@@ -0,0 +1,4 @@
+// login.js
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Login page loaded!");
+});
---
+++ Signup.js
@@ -0,0 +1,8 @@
+// signup.js
+document.addEventListener("DOMContentLoaded", function() {
+    const signupForm = document.querySelector(".signup-form");
+
+    signupForm.addEventListener("submit", function(e) {
+        alert("Signup form submitted!");
+    });
+});
